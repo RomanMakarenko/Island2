029dd45630cbd059a6ae58626d9b1e95cc34cabd
roman.makarenko@nzz.ch <roman.makarenko@nzz.ch>
2023-08-17 02:01:15 +0300

    add move version1

--- src/main/java/life/behavior/Pairing.java
+++ src/main/java/life/behavior/Pairing.java
@@ -1,4 +1,10 @@
 package life.behavior;
 
+import life.Animal;
+import life.Organism;
+
+import java.util.ArrayList;
+
 public interface Pairing {
+    public ArrayList<Animal> pair(ArrayList<Animal> organismsOnPoint);
 }
